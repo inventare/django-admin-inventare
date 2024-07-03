81222712fc83e261e97ea8ba1e34998ab1ee3b75
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-07-02 23:31:56 -0300

    chore: add version tags

--- inventare/admin_template/vendor.py
+++ inventare/admin_template/vendor.py
@@ -2,16 +2,16 @@ VENDOR_REMOTE_FILES = [
     (
         "inventare_tokens",
         "inventare/css/tokens.css",
-        "https://cdn.jsdelivr.net/npm/@inventare/tokens@latest/dist/tokens.css",
+        "https://cdn.jsdelivr.net/npm/@inventare/tokens@0.0.5/dist/tokens.css",
     ),
     (
         "inventare_ds",
         "inventare/css/inventare.css",
-        "https://cdn.jsdelivr.net/npm/@inventare/styles@latest/dist/inventare.css",
+        "https://cdn.jsdelivr.net/npm/@inventare/styles@0.0.13/dist/inventare.css",
     ),
     (
         "inventare_js",
         "inventare/js/inventare.js",
-        "https://cdn.jsdelivr.net/npm/@inventare/vanilla@latest/dist/index.js",
+        "https://cdn.jsdelivr.net/npm/@inventare/vanilla@0.0.2/dist/index.js",
     ),
 ]
